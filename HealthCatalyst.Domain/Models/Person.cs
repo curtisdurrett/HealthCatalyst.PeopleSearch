556e7d15ae63d3ae2d99a72dda136e995843526d
Curtis Durrett <curtis.durrett@gmail.com>
2017-12-03 13:29:11 -0600

    Add domain project to repos

---
+++ HealthCatalyst.Domain/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HealthCatalyst.Domain.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Interests { get; set; }
+        public string PictureFileName { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+    }
+}
